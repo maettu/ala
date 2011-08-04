9f3fc1a6b5eac7dfe2bf77c923f488976d870c2e
Matthias Bloch <matthias.bloch@puffin.ch>
2011-08-04 13:47:08 +0200

    ..and Line..

---
+++ CartesianCoordinateSystemElements/Line.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from PyQt4.QtCore import (Qt, QLineF, QRectF)
+from PyQt4.QtGui import (QGraphicsItem, QColor)
+
+class Line(QGraphicsItem):
+    """Defines a line by two points. If points are moved, line follows these movements."""
+    
+    def __init__(self, startPoint, endPoint, coordinateSystem):
+        super(Line, self).__init__(startPoint)
+        
+        self.startPoint = startPoint
+        self.endPoint = endPoint
+        self.coordinateSystem = coordinateSystem
+        
+        self.Rect = QRectF(0,0,self.endPoint.x,self.endPoint.y)
+        
+        self.color = QColor(255,0,0)
+        
+        self.rot = 100
+        
+    def boundingRect(self):
+        return self.Rect
+
+        
+        
+    def paint(self, painter, option, widget=None):        
+        painter.setPen(QColor("orange"))
+        
+        # as soon as dependent points are *not relative* to parent points any more,
+        # this needs to be changed.
+        painter.drawLine(QLineF(0, 0, self.endPoint.x, self.endPoint.y))
+        painter.drawRect(0,0, self.endPoint.x, self.endPoint.y)
+        #~ print self.startPoint.x, self.startPoint.y, self.startPoint.x+self.endPoint.x, self.startPoint.y+self.endPoint.y
