5a8a397302934d5526fb95cafe082b590a56e68b
Matthias Bloch <matthias.bloch@puffin.ch>
2011-10-06 18:06:52 +0200

    line is able to set itself invisible

--- ala/CartesianCoordinateSystemElements/Line.py
+++ ala/CartesianCoordinateSystemElements/Line.py
@@ -18,6 +18,8 @@ class Line( QGraphicsLineItem ):
         self.showIncline    = showIncline
         self.color          = color
         
+        self.visible        = True
+        
         self.minLength      =  minLength # pixel
         
         self.Rect = QRectF(self.startPoint.x, self.startPoint.y, self.endPoint.x, self.endPoint.y )
@@ -26,33 +28,39 @@ class Line( QGraphicsLineItem ):
         return self.Rect
 
     def paint( self, painter, option, widget=None ):
-        painter.setPen( QColor( self.color ) )
+        if self.visible == True:
+            painter.setPen( QColor( self.color ) )
 
-        self.sp = CST.toCcsCoord( self.ccs, self.startPoint.x, self.startPoint.y )
-        self.ep = CST.toCcsCoord( self.ccs, self.endPoint.x, self.endPoint.y )
-        
-        self.Rect = QRectF( self.sp, self.ep )
-        self.line = QLineF( self.sp, self.ep )
-        
-        if self.line.length() > self.minLength:
+            self.sp = CST.toCcsCoord( self.ccs, self.startPoint.x, self.startPoint.y )
+            self.ep = CST.toCcsCoord( self.ccs, self.endPoint.x, self.endPoint.y )
             
-            painter.drawLine( self.line )
-        
-            if self.paintToBorder == True:
+            self.Rect = QRectF( self.sp, self.ep )
+            self.line = QLineF( self.sp, self.ep )
+            
+            if self.line.length() > self.minLength:
+                
+                painter.drawLine( self.line )
+            
+                if self.paintToBorder == True:
 
-                # paint line to approximately the edge of the ccs.
-                ep2 = self.line.pointAt( self.ccs.width / self.line.length() )
-                painter.drawLine(self.ep,ep2)
-                sp2 = self.line.pointAt(-self.ccs.width / self.line.length() )
-                painter.drawLine(self.sp,sp2)
-        
-            if self.showIncline == True:
-                incline = ( self.endPoint.y - self.startPoint.y ) / ( self.endPoint.x - self.startPoint.x )
-                # print text limited to 2 decimal digits.
-                painter.setBackground ( QBrush ( QColor( 'lightGrey' ) ) )
-                painter.setBackgroundMode (Qt.BGMode(1))
-                painter.setPen( QColor( 'black' ) )
-                #~ painter.drawText( self.ep.x() + 10, self.ep.y() + 10, QString ( '%.2f' %(incline) ) )
+                    # paint line to approximately the edge of the ccs.
+                    ep2 = self.line.pointAt( self.ccs.width / self.line.length() )
+                    painter.drawLine(self.ep,ep2)
+                    sp2 = self.line.pointAt(-self.ccs.width / self.line.length() )
+                    painter.drawLine(self.sp,sp2)
+            
+                if self.showIncline == True:
+                    incline = ( self.endPoint.y - self.startPoint.y ) / ( self.endPoint.x - self.startPoint.x )
+                    # print text limited to 2 decimal digits.
+                    painter.setBackground ( QBrush ( QColor( 'lightGrey' ) ) )
+                    painter.setBackgroundMode (Qt.BGMode(1))
+                    painter.setPen( QColor( 'black' ) )
+                    #~ painter.drawText( self.ep.x() + 10, self.ep.y() + 10, QString ( '%.2f' %(incline) ) )
+        else:
+            print "no line shown"
+                
+    def setVisible( self, value ):
+        self.visible = value
         
     def updateYourself( self, xDelta, yDelta ):
         # There is no action needed, as a line gets its information
